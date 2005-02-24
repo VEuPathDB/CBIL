acd4eeab43d41d02322c391216ca905c0ac2e009
John Iodice <iodice@pennmedicine.upenn.edu>
2005-02-24 22:05:35 +0000

    Removed trailing blanks
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/trunk@3703 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Util/lib/perl/PropertySet.pm
+++ Util/lib/perl/PropertySet.pm
@@ -1,7 +1,7 @@
 package CBIL::Util::PropertySet;
 
 use strict;
- 
+
 # smart parsing of properties file.
 # file is of the format:
 # name=value
@@ -13,25 +13,25 @@ use strict;
 # if $relax is set, then allow properties in the file that are not in the declaration.
 sub new {
     my ($class, $propsFile, $propsDeclaration, $relax) = @_;
- 
+
     my $self = {};
     bless($self, $class);
- 
+
     $self->{props} = {};
     $self->{decl} = $propsDeclaration;
     $self->{file} = $propsFile;
- 
+
     foreach my $decl (@$propsDeclaration) {
         my $name = $decl->[0];
         my $value = $decl->[1];
         $self->{props}->{$name} = $value? $value : "REQD_PROP";
       }
- 
+
     if ($propsFile) {
 #      print STDERR "Reading properties from $propsFile\n";
- 
+
       open(F, $propsFile) || die "Can't open property file $propsFile";
- 
+
       while (<F>) {
         chomp;
         s/\s+$//;
@@ -39,48 +39,32 @@ sub new {
         die "Can't parse '$_' in property file '$propsFile'" unless /(\S+?)\s*=\s*(.+)/;
         my $key = $1;
         my $value = $2;
- 
+
         die "Invalid property name '$key' in property file '$propsFile'" unless $relax || $self->{props}->{$key};
- 
+
         # allow value to include $ENV{} expressions to include environment vars
         $value =~ s/\$ENV\{"?'?(\w+)"?'?\}/$ENV{$1}/g;
- 
+
         $self->{props}->{$key} = $value;
       }
       close(F);
     }
- 
+
     foreach my $name (keys %{$self->{props}}) {
         die "Required property '$name' must be specified in property file '$propsFile'"
             if ($self->{props}->{$name} eq "REQD_PROP");
     }
- 
+
     return $self;
 }
- 
+
 sub getProp {
     my ($self, $name) = @_;
- 
+
     my $value = $self->{props}->{$name};
     die "trying to call getProp('$name') on invalid property name '$name' " unless ($value ne "");
     return $value;
 }
- 
- 
+
+
 1;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
