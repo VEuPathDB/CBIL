bde3d0512f6a723320db9acd5f0083a41d6fbc7a
Brian Brunk <brunkb@upenn.edu>
2005-06-16 12:31:16 +0000

    Adding method to return all properties as a string for informational purposes and fixing a problem with revisions that I accidentally caused earlier
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/trunk@3722 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Util/lib/perl/PropertySet.pm
+++ Util/lib/perl/PropertySet.pm
@@ -6,56 +6,73 @@ use strict;
 # file is of the format:
 # name=value
 # '#' at beginning of line is a comment
-# 
+#
 # propDeclaration is a reference to an array of (name, default, comment) for
 # each property.  if default is not provided, property is required.
 #
+# if $relax is set, then allow properties in the file that are not in the declaration.
 sub new {
-    my ($class, $propsFile, $propsDeclaration) = @_;
+    my ($class, $propsFile, $propsDeclaration, $relax) = @_;
 
     my $self = {};
     bless($self, $class);
 
     $self->{props} = {};
-    $self->{help} = {};
     $self->{decl} = $propsDeclaration;
     $self->{file} = $propsFile;
 
+    my $fatalError;
+
     foreach my $decl (@$propsDeclaration) {
-	my $name = $decl->[0];
-	my $value = $decl->[1];
-	$self->{props}->{$name} = $value ? $value : "REQD_PROP";
-        $self->{help}->{$name} = $decl->[2];
-    }
+        my $name = $decl->[0];
+        my $value = $decl->[1];
+        $self->{props}->{$name} = $value? $value : "REQD_PROP";
+      }
 
     if ($propsFile) {
 #      print STDERR "Reading properties from $propsFile\n";
 
       open(F, $propsFile) || die "Can't open property file $propsFile";
 
+      my %duplicateCheck;
       while (<F>) {
-	chomp;
-	s/\s+$//;
-	next if (!$_ || /^#/);
-	die "Can't parse '$_' in props file $propsFile" unless /(\S+?)\s*=\s*(.+)/; 
-	my $key = $1;
-	my $value = $2;
+        chomp;
+        s/\s+$//;
+        next if (!$_ || /^#/);
+	if (! /(\S+?)\s*=\s*(.+)/) {
+	  print STDERR "Can't parse '$_' in property file '$propsFile'\n";
+	  $fatalError = 1;
+	}
+        my $key = $1;
+        my $value = $2;
 
-	die "Invalid property name '$key' in properties file $propsFile" unless $self->{props}->{$key};
+        if ($duplicateCheck{$key}) {
+          print STDERR "Property name '$key' is duplicated in property file '$propsFile'\n";
+          $fatalError = 1;
+	}
+        $duplicateCheck{$key} = 1;
 
-	# allow value to include $ENV{} expressions to include environment vars
-	$value =~ s/\$ENV\{"?'?(\w+)"?'?\}/$ENV{$1}/g;
+        if (!$relax && !$self->{props}->{$key}) {
+          print STDERR "Invalid property name '$key' in property file '$propsFile'\n";
+          $fatalError = 1;
+	}
 
-	$self->{props}->{$key} = $value;
+        # allow value to include $ENV{} expressions to include environment vars
+        $value =~ s/\$ENV\{"?'?(\w+)"?'?\}/$ENV{$1}/g;
+
+        $self->{props}->{$key} = $value;
       }
       close(F);
     }
 
     foreach my $name (keys %{$self->{props}}) {
-	die "Required property '$name' must be specified in $propsFile" 
-	    if ($self->{props}->{$name} eq "REQD_PROP");
-    } 
+      if ($self->{props}->{$name} eq "REQD_PROP") {
+        print STDERR "Required property '$name' must be specified in property file '$propsFile'\n";
+	$fatalError = 1;
+      }
+    }
 
+    die "Fatal PropertySet error(s)" if $fatalError;
     return $self;
 }
 
@@ -63,7 +80,7 @@ sub getProp {
     my ($self, $name) = @_;
 
     my $value = $self->{props}->{$name};
-    die "trying to call getProp('$name') on invalid property name '$name' " unless defined $value;
+    die "trying to call getProp('$name') on invalid property name '$name' " unless ($value ne "");
     return $value;
 }
 
