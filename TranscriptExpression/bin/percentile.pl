6cd41483cae4385b5c911f3792eb1aae8f0f1416
Deborah Pinney <pinney@upenn.edu>
2011-01-20 14:02:04 +0000

    move line use lib {GUS_HOME}/lib/perl so script will compile without having to adjust user's environment settings
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/CBIL/trunk@9024 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- TranscriptExpression/bin/percentile.pl
+++ TranscriptExpression/bin/percentile.pl
@@ -2,13 +2,13 @@
 
 use strict;
 
+use lib "$ENV{GUS_HOME}/lib/perl";
+
 use CBIL::TranscriptExpression::Error;
 
 use Getopt::Long;
 
 use File::Temp qw/ tempfile /;
-
-use lib "$ENV{GUS_HOME}/lib/perl";
 
 my ($help, $dataFile, $ties, $hasHeader, $outputFile, @executableDirectory);
 
