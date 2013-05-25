57980c52953dd471f6483354b9d41e524c1f2402
Sufen Hu <sufenhu@pennmedicine.upenn.edu>
2013-05-25 02:31:34 +0000

    conform to sratoolkit 2.3
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/CBIL/trunk@12189 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Util/lib/perl/Sra.pm
+++ Util/lib/perl/Sra.pm
@@ -173,7 +173,7 @@ sub getFastqForSraRunId {
     die "ERROR ($?): Unable to fetch sra file for $runId\n";
   }
   print STDERR "extracting fastq file(s)...";
-  system("fastq-dump --split-files $file");
+  system("fastq-dump --split-files ./$file");
   my @files = glob("$runId*.fastq");
   print STDERR "DONE: ".scalar(@files)." files (".join(", ",@files).")\n";
   unlink($file);
