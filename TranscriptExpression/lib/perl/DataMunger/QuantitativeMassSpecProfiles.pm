942e5d9524e090509e0496267485a0a831ab3a9b
Wei Li <weili1@pennmedicine.upenn.edu>
2013-07-16 14:46:58 +0000

    classes file and dataset for quantitative mass spec loading test
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/CBIL/branches/triTryp_bld_18@12506 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ TranscriptExpression/lib/perl/DataMunger/QuantitativeMassSpecProfiles.pm
@@ -0,0 +1,78 @@
+package CBIL::TranscriptExpression::DataMunger::QuantitativeMassSpecProfiles;
+use base qw(CBIL::TranscriptExpression::DataMunger::Profiles);
+
+use strict;
+
+use CBIL::TranscriptExpression::Error;
+
+use File::Temp qw/ tempfile /;
+
+sub new {
+  my ($class, $args) = @_;
+
+  my $requiredParams = ['makePercentiles',
+                        'profileSetName',
+                        'inputFile'
+                       ];
+
+  $args->{outputFile} = '.';
+  $args->{samples} = 'PLACEHOLDER';
+
+  my $self = $class->SUPER::new($args, $requiredParams);
+
+  $self->{profileSetDescription} = $self->getProfileSetName();
+
+  return $self;
+}
+
+
+sub munge {
+  my ($self) = @_;
+
+  my $outputFile = $self->getInputFile();
+
+  my $makePercentiles = $self->getMakePercentiles;
+
+  my ($tempFh, $tempFn) = tempfile();
+
+  my $pctOutputFile = $outputFile . ".pct";
+
+  my $header = 'TRUE';
+
+  my $rString = <<RString;
+
+source("$ENV{GUS_HOME}/lib/R/TranscriptExpression/profile_functions.R");
+
+if($makePercentiles) {
+
+
+    dat = read.table("$outputFile", header=$header, sep="\\t", check.names=FALSE, row.names=1);
+    dat.samples = list();
+    res = list(id=NULL, data=NULL);
+    res\$id = as.vector(dat[,1]);
+    groupNames = row.names(dat);
+    groupNames = paste("ID\t", groupNames, sep="");
+    
+    dat\$percentile = percentileMatrix(m=dat);
+    write.table(dat\$percentile, file="$pctOutputFile",quote=F,sep="\\t", row.names=groupNames);
+}
+
+quit("no");
+RString
+
+  print $tempFh $rString;
+
+  $self->runR($tempFn);
+
+  unlink($tempFn);
+
+  my $doNotLoad = $self->getDoNotLoad(); 
+  unless($doNotLoad){
+    $self->createConfigFile();
+    if($self->getIsTimeSeries() ){
+      $self->createTimeSeriesConfigFile();
+    }
+}	
+}
+1;
+ 
