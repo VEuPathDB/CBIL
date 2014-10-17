2d39e2494da44dd61133b67c11df721692763322
John Brestelli <jbrestell@gmail.com>
2014-10-17 19:14:04 +0000

    remove deprecated quant mass spec module.  clean up Generic profiles
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/CBIL/branches/4.0@14854 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- TranscriptExpression/lib/perl/DataMunger/GenericProfiles.pm
+++ TranscriptExpression/lib/perl/DataMunger/GenericProfiles.pm
@@ -13,7 +13,6 @@ sub new {
   my ($class, $args) = @_;
 
   my $requiredParams = ['makePercentiles',
-                        'profileSetName',
                         'inputFile'
                        ];
 
@@ -48,63 +47,12 @@ sub new {
 
   my $self = $class->SUPER::new($args, $requiredParams);
 
-  $self->{profileSetDescription} = $self->getProfileSetName();
-
   return $self;
 }
 
 
-sub munge {
-  my ($self) = @_;
 
-  my $inputFile = $self->getOutputFile();
-  my $outputFile = $self->getOutputFile();
 
-  my $makePercentiles = $self->getMakePercentiles;
 
-  my ($tempFh, $tempFn) = tempfile();
-
-  my $pctOutputFile = $outputFile . ".pct";
-
-  my $header = 'TRUE';
-
-  my $samples = $self->makeSamplesRString();
-
-  
-  my $rString = <<RString;
-
-source("$ENV{GUS_HOME}/lib/R/TranscriptExpression/profile_functions.R");
-
-if($makePercentiles) {
-
-    dat = read.delim("$outputFile", header=$header, sep="\\t", check.names=FALSE);
-
-   dat.samples = list();
-	$samples
-
-    reorderedSamples = reorderAndGetColCentralVal(pl=dat.samples, df=dat);
-
-    reorderedSamples\$percentile = percentileMatrix(m=reorderedSamples\$data);
-    write.table(reorderedSamples\$percentile, file="$pctOutputFile",quote=F,sep="\\t",row.names=reorderedSamples\$id, col.names=NA);
-}
-
-quit("no");
-RString
-
-  print $tempFh $rString;
-
-  $self->runR($tempFn);
-
-  unlink($tempFn);
-
-  my $doNotLoad = $self->getDoNotLoad(); 
-  unless($doNotLoad){
-    $self->createConfigFile();
-    if($self->getIsTimeSeries() ){
-      # TODO:  Write param for is Time Series
-      #$self->createTimeSeriesConfigFile();
-    }
-}	
-}
 1;
  
