638bc3f9f0ed80afec88521d6c21ff1fc8a6cc66
Ja’Shon Cade <jcade@upenn.edu>
2016-05-09 19:34:21 +0000

    added method to create mapping file for cases when no mapping is required
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/CBIL/branches/4.0@18430 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- TranscriptExpression/lib/perl/DataMunger/NoSampleConfigurationProfiles.pm
+++ TranscriptExpression/lib/perl/DataMunger/NoSampleConfigurationProfiles.pm
@@ -10,7 +10,10 @@ use CBIL::TranscriptExpression::Error;
 
 use Data::Dumper;
 
+use File::Basename;
+
 use File::Temp qw/ tempfile /;
+
 
 sub new {
   my ($class, $args) = @_;
@@ -20,7 +23,7 @@ sub new {
                        ];
 
   $args->{outputFile} = $args->{inputFile};
-  
+
   my $output = $args->{outputFile};
   
   unless ($args->{isLogged}) {
@@ -46,6 +49,7 @@ sub new {
     die "sample names must be unique, average samples with the profiles step class before calling this step class";
   }
   $args->{samples} = \@samples;
+  $args->{doNotLoad} = 1;
 
 
   my $self = $class->SUPER::new($args, $requiredParams);
@@ -53,9 +57,58 @@ sub new {
   return $self;
 }
 
+sub munge {
+  my ($self) = @_;
+  
+  $self->{mappingFile} = $self->getMappingFile();
+  $self->SUPER::munge();
+
+}
+
+sub getMappingFile {
+  my ($self) = @_;
+  
+  my $inputFile = $self->{inputFile};
+
+  if($self->inputFileIsMappingFile()) {
+    print STDERR "Using Input File\n";
+    return $inputFile;
+  }
+  
+  if (defined $self->{mappingFile}) {
+    print STDERR "Using Mapping File\n";
+    return $self->{mappingFile};
+  }
+  my $mappingFile = $self->makeSelfMappingFile();
+      print STDERR "Using $mappingFile \n";
+  return $mappingFile;
+  
+}
 
 
-
+sub makeSelfMappingFile {
+  my ($self) = @_;
+  my $inputFile = $self->{inputFile};
+  my $mainDirectory = $self->{mainDirectory};
+  print STDERR " my main directory is $mainDirectory\n";
+  my $mappingFile = $mainDirectory."/geneProbeMapping.tab";
+  open (INPUT, "<$inputFile") or die "unable to open input file $inputFile : $!";
+  open (MAPPING, ">$mappingFile") or die "unable to open input file $inputFile : $!";
+  my $isHeader = 1;
+  while(<INPUT>) {
+    unless ($isHeader) {
+      my ($id) = split(/\t/,$_);
+      print MAPPING "$id\t$id\n";
+    }
+    else {
+      $isHeader = 0;
+      next;
+    }
+  }
+  close INPUT;
+  close MAPPING;
+  return $mappingFile;
+}
 
 1;
  
