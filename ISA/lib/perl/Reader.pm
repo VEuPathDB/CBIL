415aa4c743e07e05a22cbf0f8503b4f5f9de1fe5
Jay Humphrey <jaycolin@apidb.org>
2019-10-18 15:43:22 -0400

    CBIL::ISA::Reader provides basic methods for parsing ISA standard files in tab-delimited format

---
+++ ISA/lib/perl/Reader.pm
@@ -0,0 +1,47 @@
+package CBIL::ISA::Reader;
+use Text::CSV_XS;
+use strict;
+use warnings;
+
+
+sub getFh {$_[0]->{_fh}}
+sub setFh {$_[0]->{_fh} = $_[1]}
+sub setDelimiter { $_[0]->{_delimiter} = $_[1] }
+sub getDelimiter { $_[0]->{_delimiter} }
+sub setLineParser { $_[0]->{_line_parser} = $_[1] }
+sub getLineParser { $_[0]->{_line_parser} }
+
+sub initReader {
+  my ($self, $delimiter) = @_;
+  $self->setDelimiter($delimiter);
+  my $csv = Text::CSV_XS->new({
+    binary => 1,
+    sep_char => $delimiter,
+    quote_char => '"'
+  }) or die "Cannot use CSV: " . Text::CSV_XS->error_diag ();
+  $self->setLineParser($csv);
+}
+
+sub readNextLine {
+  my ($self) = @_;
+  my $fh = $self->getFh();
+  my $csv = $self->getLineParser();
+  my $fields = $csv->getline($fh);
+  return $fields;
+}
+
+sub splitLine {
+  my ($self, $line) = @_;
+  my $csv = $self->getLineParser();
+  my @columns;
+  if($csv->parse($line)) {
+    @columns = $csv->fields();
+  }
+  else {
+    my $error= "" . $csv->error_diag;
+    die "Could not parse line: $error";
+  }
+  return wantarray ? @columns : \@columns;
+}
+
+1;
