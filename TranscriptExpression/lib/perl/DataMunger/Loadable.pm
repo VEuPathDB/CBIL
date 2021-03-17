c55e0e382ed5c52c2889bacf42add390491c202a
John Brestelli <jbrestel@upenn.edu>
2021-03-17 11:20:40 -0400

    do not allow invalid characters;  currently only test for comma in sample and profileset names

--- TranscriptExpression/lib/perl/DataMunger/Loadable.pm
+++ TranscriptExpression/lib/perl/DataMunger/Loadable.pm
@@ -121,6 +121,11 @@ sub createConfigFile {
     # don't allow the something to be input to itself
     $inputProtocolAppNodesString = undef if($inputProtocolAppNodesString eq $name);
 
+    my $invalidChars = qr/,/;
+    if($profileSetName =~ $invalidChars || $name =~ $invalidChars) {
+      CBIL::TranscriptExpression::Error->new("Invalid Character in either sample or profileset")->throw();
+    }
+
      $self->printConfigLine(\*CONFIG, $profileSetName, $name, $fileName, $inputProtocolAppNodesString);
    }
 
